ddea41dba2cb222cacc3b519959a5596dbbb5a34
Jeremi Aron <chancanjeremiaron@gmail.com>
2023-01-02 20:49:14 -0500

    auto-season

--- src/context/DataApiContext.jsx
+++ src/context/DataApiContext.jsx
@@ -1,13 +1,29 @@
 import React, { useState, useEffect, createContext } from "react";
 import { infoData } from "./fetch";
-import Loading from '../components/Loading';
+import Loading from "../components/Loading";
 
 const dataApi = createContext(null);
+const dateNow = new Date();
+const month = dateNow.getMonth();
+const year = dateNow.getFullYear();
+
+function getSeason(x) {
+    switch (true) {
+    case x <= 2:
+        return "winter";
+    case x <= 5:
+        return "spring";
+    case x <= 8:
+        return "summer";
+    case x <= 11:
+        return "fall";
+    }
+}
 
 function DataApiContext({ children }) {
   const [data, setData] = useState([]);
   const link =
-    "https://kitsu.io/api/edge/anime?filter[season]=fall&filter[seasonYear]=2022&filter[subtype]=TV&sort=popularityRank&page[limit]=20";
+    `https://kitsu.io/api/edge/anime?filter[season]=${getSeason(month)}&filter[seasonYear]=${year}&filter[subtype]=TV&sort=popularityRank&page[limit]=20`;
 
   useEffect(() => {
     (async () => {
@@ -17,14 +33,10 @@ function DataApiContext({ children }) {
   }, []);
 
   if (!(data.length > 0)) {
-    return <Loading />
+    return <Loading />;
   }
 
-  return (
-    <dataApi.Provider value={data}>
-      {children}
-    </dataApi.Provider>
-  );
+  return <dataApi.Provider value={data}>{children}</dataApi.Provider>;
 }
 
 export { DataApiContext, dataApi };
